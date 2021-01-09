40981a673605fef691f72e1a19892f00cb27e43f
lewisseo91 <togokr@naver.com>
2021-01-09 14:47:04 +0900

    feat : 메인 생성
     - 메인 생성

---
+++ src/main/java/baseball/Main.java
@@ -0,0 +1,13 @@
+package baseball;
+
+import baseball.domain.Balls;
+import baseball.view.BaseballView;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BaseballView.printGameStart();
+    }
+}
