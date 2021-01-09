7c384d546ec3f5690ee84550a13a055f5dec8648
lewisseo91 <togokr@naver.com>
2021-01-09 11:38:05 +0900

    feat : Ball 기능 생성
     - Ball 기능 생성
     - BallStatus 완성
     - Ball compare 기능 완성

--- src/main/java/baseball/Ball.java
+++ src/main/java/baseball/Ball.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import java.util.Objects;
+
 public class Ball {
     private int pos;
     private int val;
@@ -9,7 +11,33 @@ public class Ball {
         this.val = val;
     }
 
-    public int compare(Ball userBall) {
-        return 0;
+    public BallStatus compare(Ball userBall) {
+        if(userBall.equals(this)) {
+            return BallStatus.STRIKE;
+        }
+
+        if(userBall.matchBallVal(this.val)) { /* 중요 */
+            return BallStatus.BALL;
+        }
+
+        return BallStatus.NOTHING;
+    }
+
+    private boolean matchBallVal(int val) {
+        return this.val == val;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ball ball = (Ball) o;
+        return pos == ball.pos &&
+                val == ball.val;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pos, val);
     }
 }
--- src/main/test/baseball/BallTest.java
+++ src/main/test/baseball/BallTest.java
@@ -6,10 +6,24 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class BallTest {
     @Test
+    public void strikeTest () {
+        Ball comBall = new Ball(1, 1);
+        Ball userBall = new Ball(1, 1);
+        BallStatus ballStatus = comBall.compare(userBall);
+        assertThat(ballStatus).isEqualTo(BallStatus.STRIKE);
+    }
+    @Test
     public void ballTest () {
         Ball comBall = new Ball(0, 1);
         Ball userBall = new Ball(1, 1);
-        BallStatus.BALL
-        assertThat(comBall.compare(userBall)).isEqualTo(1);
+        BallStatus ballStatus = comBall.compare(userBall);
+        assertThat(ballStatus).isEqualTo(BallStatus.BALL);
+    }
+    @Test
+    public void nothingTest () {
+        Ball comBall = new Ball(0, 1);
+        Ball userBall = new Ball(0, 0);
+        BallStatus ballStatus = comBall.compare(userBall);
+        assertThat(ballStatus).isEqualTo(BallStatus.NOTHING);
     }
 }
