d71f4080ad8557455737d4995b7528850ed7078c
lewisseo91 <togokr@naver.com>
2021-01-09 15:36:31 +0900

    feat : 랜덤 숫자 생성
     - generateDistinctRandomNumber 추가
     - playGame 로직 분리

--- src/main/java/baseball/Main.java
+++ src/main/java/baseball/Main.java
@@ -1,13 +1,34 @@
 package baseball;
 
 import baseball.domain.Balls;
+import baseball.domain.PlayResult;
 import baseball.view.BaseballView;
 
-import java.util.List;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
 
-        BaseballView.printGameStart();
+    public static List<Integer> generateDistinctRandomNumber() {
+        List<Integer> origin = new ArrayList<>(List.of(0, 1, 2, 3, 4, 5, 6, 8, 9));
+        Collections.shuffle(origin);
+        return origin.subList(0, Balls.BALL_COUNT);
     }
+
+    public static void main(String[] args) {
+        BaseballView.printGameStart();
+        do {
+            playGame();
+        } while (BaseballView.printWinner());
+    }
+
+    private static void playGame() {
+        Balls comBalls = new Balls(generateDistinctRandomNumber());
+        PlayResult playResult;
+        do {
+            Balls userBalls = new Balls(BaseballView.inputBalls());
+            playResult = userBalls.play(comBalls);
+            BaseballView.printPlayResultCount(playResult);
+        } while(!playResult.isEndGame());
+    }
+
 }
