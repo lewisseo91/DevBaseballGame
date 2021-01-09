500b5e8bbec3e1cf83981fa3b328f5f3b5223687
lewisseo91 <togokr@naver.com>
2021-01-09 11:21:27 +0900

    feat : Ball 생성, Ball compare 생성
     - Ball 생성
     - Ball compare 생성

---
+++ src/main/java/baseball/Ball.java
@@ -0,0 +1,15 @@
+package baseball;
+
+public class Ball {
+    private int pos;
+    private int val;
+
+    public Ball(int pos, int val) {
+        this.pos = pos;
+        this.val = val;
+    }
+
+    public int compare(Ball userBall) {
+        return 0;
+    }
+}
--- src/main/test/baseball/BallTest.java
+++ src/main/test/baseball/BallTest.java
@@ -1,4 +1,15 @@
-package main.test.baseball;
+package baseball;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class BallTest {
+    @Test
+    public void ballTest () {
+        Ball comBall = new Ball(0, 1);
+        Ball userBall = new Ball(1, 1);
+        BallStatus.BALL
+        assertThat(comBall.compare(userBall)).isEqualTo(1);
+    }
 }
